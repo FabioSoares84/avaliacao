f44f93390a5eb106c4319ac59c4e6fa30cad29c5
FabioSoares84 <binhoblz@hotmail.com>
2021-04-07 16:46:06 -0300

    Enviando Pasta do Bootstrap

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>AVALIAÇÂO FABIO SOARES</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
         <?php
